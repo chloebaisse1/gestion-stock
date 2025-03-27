5175ea3804a6c95e771dcdfe702edd377d0938ac
chloebaisse1 <baissechloe@gmail.com>
2025-03-27 14:21:07 +0400

    creation bdd

--- types/types.ts
+++ types/types.ts
@@ -5,3 +5,14 @@ export interface BookFormData {
   price: number
   stock: number
 }
+
+export interface Book extends BookFormData {
+  id: number
+}
+
+export interface BookModalProps {
+  isOpen: boolean
+  onClose: () => void
+  onSubmit: (data: BookFormData) => void
+  bookToEdit?: Book
+}
